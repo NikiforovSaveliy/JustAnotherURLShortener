5ce49c8d50f49523b907ddf1cbaa91374ee87906
Nikiforov Saveliy <113004532+NikiforovSaveliy@users.noreply.github.com>
2023-07-23 21:48:39 +0300

    fix: model issues

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from peewee import SqliteDatabase
+from models import Link
 
 app = Flask(__name__)
-
 db = SqliteDatabase('links')
 
+Link.create_table()
 from .views import *
